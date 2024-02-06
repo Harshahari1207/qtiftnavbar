03da1f38da4b1259bc2883f1b4733b2d2e54e7df
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2024-02-06 14:14:07 +0530

    UI of navbar.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+      <NavBar />
     </div>
   );
 }
