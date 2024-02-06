48e2f7583e8ec2cee4161c1a0a97c90bf3fb8924
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2024-02-06 11:20:20 +0530

    Basic UI of NavBar.

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+export default function NavBar(){
+    return(
+        <div>
+            <div className="logo">
+                <img src=""/>
+            </div>
+            <div className="search">
+                <input type="text" name="search" id="search" />
+                <button>Search</button>
+            </div>
+            <div className="">
+                <ul>
+
+                </ul>
+            </div>
+        </div>
+    )
+}
