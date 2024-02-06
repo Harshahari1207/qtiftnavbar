4a6866ec60e8b6c2ceab3eb74b0ce6b277c7cd0c
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2024-02-06 18:30:42 +0530

    Hero section.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import HeroSection from "./components/HeroSection";
 import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="hero-main">
+        <HeroSection/>
+      </div>
     </div>
   );
 }
